2f0dc80befa03de4fd2c3509249b8fde6e03cf02
carlaescrigduran <carlaescrigduran@gmail.com>
2026-04-16 10:38:12 +0200

    commit

--- app.js
+++ app.js
@@ -569,7 +569,7 @@ function renderReel() {
   const wrapper = document.createElement("div");
   wrapper.className = "media";
   wrapper.innerHTML = `
-    <iframe src="https://player.vimeo.com/video/1183662839" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" allowfullscreen></iframe>
+    <iframe title="vimeo-player" src="https://player.vimeo.com/video/1183662839?h=18cb79160b" width="640" height="360" frameborder="0" referrerpolicy="strict-origin-when-cross-origin" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" allowfullscreen></iframe>
   `;
 
   reel.appendChild(wrapper);
